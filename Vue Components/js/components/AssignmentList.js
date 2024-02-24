44c590c086612fb161d28cdd4982e85a8b310249
Udayanga <udayanga.wijesiri@gmail.com>
2024-02-24 09:03:46 +0530

    Move assignment tags into a separate component

--- Vue Components/js/components/AssignmentList.js
+++ Vue Components/js/components/AssignmentList.js
@@ -1,7 +1,8 @@
 import Assignment from "./Assignment.js"
+import AssignmentTags from "./AssignmentTags.js"
 
 export default {
-	components: { Assignment },
+	components: { Assignment, AssignmentTags },
 
 	template: `
     <section v-show="assignments.length">
@@ -11,14 +12,12 @@ export default {
 
       </h2>
 
-      <div class="flex gap-2">
-        <button
-          @click="currentTag = tag"
-          v-for="tag in tags" 
-          class="border rounded px-1 py-px text-xs"
-          :class="{ 'border-blue-500 text-blue-500': tag === currentTag }"
-        >{{ tag }}</button>
-      </div>
+      <assignment-tags
+        :initial-tags="assignments.map(a => a.tag)"
+        :current-tag="currentTag"
+        @change="currentTag = $event"  
+      />
+        
 
       <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
         <assignment 
@@ -48,10 +47,6 @@ export default {
 			}
 
 			return this.assignments.filter((a) => a.tag === this.currentTag)
-		},
-
-		tags() {
-			return ["all", ...new Set(this.assignments.map((a) => a.tag))]
 		},
 	},
 }
