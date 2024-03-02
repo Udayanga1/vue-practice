b57d2ada11ffbf08de2f48fb1932861b5cdfee5e
Udayanga <udayanga.wijesiri@gmail.com>
2024-03-02 18:26:34 +0530

    shopping-list-app: user events

--- shopping-list-app/src/App.vue
+++ shopping-list-app/src/App.vue
@@ -14,22 +14,27 @@ const iceCreamFlavors = ref([])
 
 <template>
 	<h1 class="text-4xl mb-4 font-bold">{{ header }}</h1>
-	<input
-		class="px-2 py-1 mr-2"
-		v-model.trim="newItem"
-		type="text"
-		placeholder="Add an item"
-	/>
-	<label>
-		<input class="m-1" type="checkbox" v-model="newItemHighPriority" />
-		<span>High Priority:</span>
-	</label>
+	<form @submit.prevent="items.push({ id: items.length + 1, label: newItem })">
+		<input
+			class="px-2 py-1 mr-2"
+			v-model.trim="newItem"
+			type="text"
+			placeholder="Add an item"
+		/>
+		<label>
+			<input class="m-1" type="checkbox" v-model="newItemHighPriority" />
+			<span>High Priority</span>
+		</label>
+		<br />
+		<button
+			class="bg-blue-400 px-3 py-2 rounded-lg text-white font-bold text-lg mt-3"
+		>
+			Save Item
+		</button>
+	</form>
 	<ul class="mt-2">
 		<li class="leading-loose text-xl" v-for="{ id, label } in items" :key="id">
 			{{ label }}
-		</li>
-		<li class="leading-loose text-xl">
-			{{ newItem }}
 		</li>
 	</ul>
 </template>
