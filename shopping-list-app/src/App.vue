03fcce24ba32a8ab53f4e46c7df72b981ae2eef2
Udayanga <udayanga.wijesiri@gmail.com>
2024-03-04 12:44:24 +0530

    shopping-list-app: conditionals

--- shopping-list-app/src/App.vue
+++ shopping-list-app/src/App.vue
@@ -2,23 +2,44 @@
 import { ref } from "vue"
 
 const header = ref("Shopping List App")
+const editing = ref(false)
 const items = ref([
-	{ id: 1, label: "10 party hats" },
-	{ id: 2, label: "20 balloons" },
-	{ id: 3, label: "5 candles" },
+	// { id: 1, label: "10 party hats" },
+	// { id: 2, label: "20 balloons" },
+	// { id: 3, label: "5 candles" },
 ])
 const newItem = ref("")
 const newItemHighPriority = ref(false)
-const iceCreamFlavors = ref([])
 const saveItem = () => {
 	items.value.push({ id: items.value.length + 1, label: newItem.value })
+	newItem.value = ""
+}
+const doEdit = (e) => {
+	editing.value = e
 	newItem.value = ""
 }
 </script>
 
 <template>
-	<h1 class="text-4xl mb-4 font-bold">{{ header }}</h1>
-	<form @submit.prevent="saveItem">
+	<div>
+		<h1 class="text-4xl mb-4 font-bold">{{ header }}</h1>
+		<button
+			v-if="editing"
+			@click="doEdit(false)"
+			class="bg-gray-400 px-3 py-2 rounded-lg text-white font-bold text-lg"
+		>
+			Cancel
+		</button>
+
+		<button
+			v-else
+			@click="doEdit(true)"
+			class="bg-blue-400 px-3 py-2 rounded-lg text-white font-bold text-lg"
+		>
+			Add Item
+		</button>
+	</div>
+	<form v-if="editing" @submit.prevent="saveItem">
 		<input
 			class="px-2 py-1 mr-2"
 			v-model.trim="newItem"
@@ -40,5 +61,6 @@ const saveItem = () => {
 		<li class="leading-loose text-xl" v-for="{ id, label } in items" :key="id">
 			{{ label }}
 		</li>
+		<p v-if="!items.length">Nothing to see here</p>
 	</ul>
 </template>
